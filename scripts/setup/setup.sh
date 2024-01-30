b78bcfd8acb69364087dc560d6edf3c9aaa7475c
Finn Banks <banks.finn@gmail.com>
2024-01-29 21:13:37 -0800

    added in better instructions

---
+++ scripts/setup/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o pipefail
+
+echo "Generating script virtual environment (python)"
+python3 -m venv scripts/venv
+source scripts/venv/bin/activate
+pip install -r scripts/requirements.txt
+
+echo "Generating gateway virtual environment (python)"
+python3 -m venv gateway/venv
+source gateway/venv/bin/activate
+pip install -r gateway/requirements.txt
